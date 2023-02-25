4d904442d9a110c8a8e92b7499e7454e67b47b0e
YMostagi <yuri.mostagi@gmail.com>
2023-02-25 17:14:03 -0300

    Criando class Livro.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,9 +1,6 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
-
-import java.sql.SQLOutput;
-
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
@@ -17,32 +14,25 @@ public class PrimeiroPrograma {
 class Livro {
     private String nome;
     private Integer numPaginas;
-
     public Livro(String nome, Integer numPaginas) {
         this.nome = nome;
         this.numPaginas = numPaginas;
     }
-
     public Livro(String nome) {
         this.nome = nome;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public Integer getNumPaginas() {
         return numPaginas;
     }
-
     public void setNumPaginas(Integer numPaginas) {
         this.numPaginas = numPaginas;
     }
-
     @Override
     public String toString() {
         return "Livro{" +
